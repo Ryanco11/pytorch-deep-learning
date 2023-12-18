60cc5fae627fad8c4e6feeaf0c8052fd4260dfd6
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-18 22:24:02 +0800

    table of conten

--- tutorial/chapter01.py
+++ tutorial/chapter01.py
@@ -1,5 +1,20 @@
+# pytorch end-to-end workflow
+
+# what_were_covering = {
+#     1:"data (prepare and load)",
+#     2:"build model",
+#     3:"fitting the model to data (training)",
+#     4:"making predictions and evaluting a model (inference)",
+#     5:"saving and loading a model",
+#     6:"putting it all together"
+# }
+
+
 import torch
-import pandas as pd
-import numpy as np
+from torch import nn # nn contains all of pytorch's building blocks for neural networks
 import matplotlib.pyplot as plt
+
+
+print(torch.__version__)
+
 

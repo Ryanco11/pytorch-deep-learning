3249f56b1546c762ad1490f943161e8103157ef9
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-18 20:42:40 +0800

    add tut file

---
+++ tutorial/tutorial.py
@@ -0,0 +1,50 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(torch.__version__)
+
+scalar = torch.tensor(7)
+print(scalar)
+# tensor(7)
+print(scalar.ndim)
+# 0
+print(scalar.item())
+# 7
+
+vector = torch.tensor([7, 7])
+print(vector)
+# tensor([7, 7])
+print(vector.ndim)
+# 1
+print(vector.shape)
+# torch.Size([2])
+
+MATRIX = torch.tensor([[7, 8],
+                       [9, 10]])
+print(MATRIX)
+# tensor([[ 7,  8],
+#        [ 9, 10]])
+print(MATRIX.ndim)
+# 2
+print(MATRIX[1])
+# tensor([ 9, 10])
+print(MATRIX.shape)
+# torch.Size([2, 2])
+
+TENSOR = torch.tensor([[[1, 2, 3],
+                        [3, 6, 9], 
+                        [2, 4, 5]]])
+print(TENSOR)
+# tensor([[[1, 2, 3],
+        #  [3, 6, 9],
+        #  [2, 4, 5]]])
+print(TENSOR.ndim)
+# 3
+print(TENSOR.shape)
+# torch.Size([1, 3, 3])
+print(TENSOR[0])
+# tensor([[1, 2, 3],
+        # [3, 6, 9],
+        # [2, 4, 5]])

eef85c55344828dc1bda048ad7072822b4b06152
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-18 21:44:54 +0800

    get information from tensor

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -103,3 +103,14 @@ print(float_16_tensor.dtype)
 # int tensor
 int_32_tensor = torch.tensor([3, 6, 9], dtype=torch.long)
 print(int_32_tensor * float_32_tensor)
+
+
+
+# get information from tensor
+# tensor.dtype
+# tensor.shape
+# tensor.device
+some_tensor = torch.rand(3, 4)
+print(some_tensor.dtype)
+print(some_tensor.shape)
+print(some_tensor.device)

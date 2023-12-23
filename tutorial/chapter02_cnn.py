81f2f06a53c902905788548c04be3fec80a9923a
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-23 22:00:31 +0800

    visualize

--- tutorial/chapter02_cnn.py
+++ tutorial/chapter02_cnn.py
@@ -21,10 +21,17 @@ print(f"First 5 samples of X:\n {y[:5]}")
 
 
 # make dataframe of circle data
-
 import pandas as pd
 circles = pd.DataFrame({"X1":X[:, 0],
                         "X2":X[:, 1],
                         "label":y})
 print(circles.head(10))
 
+
+# visualize
+import matplotlib.pyplot as plt
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y,
+            cmap=plt.cm.RdYlBu)
+plt.show()

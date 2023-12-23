d07a7189bd66f26b0e6ff37367bf2b3c22ae016b
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-23 21:58:16 +0800

    make dataframe

--- tutorial/chapter02_cnn.py
+++ tutorial/chapter02_cnn.py
@@ -18,3 +18,13 @@ print(len(X), len(y))
 
 print(f"First 5 samples of X:\n {X[:5]}")
 print(f"First 5 samples of X:\n {y[:5]}")
+
+
+# make dataframe of circle data
+
+import pandas as pd
+circles = pd.DataFrame({"X1":X[:, 0],
+                        "X2":X[:, 1],
+                        "label":y})
+print(circles.head(10))
+

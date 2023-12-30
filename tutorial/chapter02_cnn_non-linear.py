ffc168cc60d2325670c31507173f07b57d7a43fc
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-30 11:09:50 +0800

    non linear data

--- tutorial/chapter02_cnn_non-linear.py
+++ tutorial/chapter02_cnn_non-linear.py
@@ -2,6 +2,10 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 
+import torch
+from sklearn.model_selection import train_test_split
+
+
 if __name__ == "__main__":
 
     n_samples = 1000
@@ -12,4 +16,15 @@ if __name__ == "__main__":
     )
 
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
-    plt.show()
+    # plt.show()
+
+    X = torch.from_numpy(X).type(torch.float)
+    y = torch.from_numpy(y).type(torch.float)
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=42)
+
+    print(X_train[:5], y_train[:5])

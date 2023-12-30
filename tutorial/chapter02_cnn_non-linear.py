6ca73f24b75f53580a65be237a8ffade5377005d
DESKTOP-LQ71V0C\Ashley <ashley.com>
2023-12-30 20:51:30 +0800

    save

--- tutorial/chapter02_cnn_non-linear.py
+++ tutorial/chapter02_cnn_non-linear.py
@@ -6,10 +6,54 @@ import torch
 from torch import nn
 from sklearn.model_selection import train_test_split
 
+import numpy as np
+
 
 # setup device agonistic code to run cuda
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"device: {device}")
+
+# calculate accuracy
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
+
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    """Plots decision boundaries of model predicting on X in comparison to y.
+
+    Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
+    """
+    # Put everything to CPU (works better with NumPy + Matplotlib)
+    model.to("cpu")
+    X, y = X.to("cpu"), y.to("cpu")
+
+    # Setup prediction boundaries and grid
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+
+    # Test for multi-class or binary and adjust logits to prediction labels
+    if len(torch.unique(y)) > 2:
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
+    else:
+        y_pred = torch.round(torch.sigmoid(y_logits))  # binary
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
 
 if __name__ == "__main__":
 
@@ -40,12 +84,12 @@ if __name__ == "__main__":
             super().__init__()
             self.layer_1 = nn.Linear(in_features=2, out_features=10)
             self.layer_2 = nn.Linear(in_features=10, out_features=10)
-            self.layer_3 = nn.Linear(in_features=10, out_features=10)
+            self.layer_3 = nn.Linear(in_features=10, out_features=1)
             self.relu = nn.ReLU()
 
         def forward(self, x):
-            # return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(X)))))
-            return self.layer_2(self.layer_1(x)) # x-> layer_1 -> layer_2 -> output
+            return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(X)))))
+            # return self.layer_3(self.layer_2(self.layer_1(x))) # x-> layer_1 -> layer_2 -> output
     
 
     model_3 = CircleModelV2().to(device)
@@ -117,10 +161,10 @@ if __name__ == "__main__":
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
     plt.title("Train")
-    plot_decision_boundary(model_1, X_train, y_train) # model_1 = no non-linearity
+    plot_decision_boundary(model_3, X_train, y_train) # model_1 = no non-linearity
     plt.subplot(1, 2, 2)
     plt.title("Test")
     plot_decision_boundary(model_3, X_test, y_test) # model_3 = has non-linearity
-    plot.show()
+    plt.show()
 
     
